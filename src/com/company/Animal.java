c0f9421dfb3df76e88db8811fb03b225b767badd
Brandon Carpenter <Carpbra3@isu.edu>
2017-09-04 17:59:32 -0600

    Add classes for Animals example

---
+++ src/com/company/Animal.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import static java.lang.System.*;
+
+/**
+ * Created by Brandon on 8/5/2017.
+ */
+public class Animal {
+
+    private String name;
+    private double height;
+    private int weight;
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public void setHeight(double newHeight){
+        height = newHeight;
+    }
+
+
+}
---
+++ src/com/company/Cat.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by Brandon on 8/5/2017.
+ */
+public class Cat extends Animal{
+
+
+}
